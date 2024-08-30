2cf0c89ce444e3ea1713fcaedec23f80652dc922
GauravTendulkar26 <gauravtendulkar1165@gmail.com>
2024-08-30 11:07:53 +0530

    add file

--- first.py
+++ first.py
@@ -3,5 +3,18 @@
 
 var = 3
 
-print(type(var))  # <class 'int'>
+# print(type(var))  # <class 'int'>
 
+
+# print(5 * "gaurav\n")
+
+# type casting 
+# print(int("10")+int("10"))
+# print(int("10")+float("10"))
+
+#  add two by giving output
+
+sum1 = input("Enter the number")
+sum2 = input("Enter the number")
+
+print(int(sum1) + int(sum2))
--- swap.py
+++ swap.py
@@ -8,3 +8,5 @@ numb2 = temp
 print("numb1 is "+numb1)
 print("numb2 is"+numb2)
 
+
+
